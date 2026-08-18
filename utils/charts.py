8de5fa550dc3d2eb644fcb75bee8ca14ad183ff7
Dokkari Sowjanya <sowjanyadokkari@gmail.com>
2026-08-18 17:05:47 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -5,23 +5,19 @@ def create_pie_chart(matched, missing):
     labels = ["Matched", "Missing"]
     sizes = [len(matched), len(missing)]
 
-    if sum(sizes) == 0:
-        sizes = [1, 1]
-
-    fig, ax = plt.subplots(figsize=(2, 2))
+    fig, ax = plt.subplots(figsize=(2.2, 2.2))
 
     ax.pie(
         sizes,
         labels=labels,
         autopct="%1.0f%%",
         startangle=90,
-        radius=0.9,
-        textprops={"fontsize": 10}
+        radius=0.8,
+        textprops={"fontsize": 9}
     )
 
-    ax.set_title("Skill Analysis", fontsize=11)
-    plt.tight_layout(pad=0.2)
+    ax.set_title("Skill Analysis", fontsize=10, pad=5)
 
-   
+    plt.tight_layout(pad=0.3)
 
     return fig
